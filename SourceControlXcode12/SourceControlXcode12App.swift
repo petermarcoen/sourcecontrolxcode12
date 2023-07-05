6a74d70998f554cea16ce6402a04cf10dcbf3fc2
Peter Marcoen <pmarcoen@gmail.com>
2023-07-05 10:39:21 +0200

    Initial Commit

---
+++ SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by Peter Marcoen on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
